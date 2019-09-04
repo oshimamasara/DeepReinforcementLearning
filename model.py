1760dc91bc2b89dcc3e391318a2263570fa3ea3b
oshimamasara <oshimamasara@gmail.com>
2019-09-05 02:53:00 +0900

    Update model.py

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ import loggers as lg
 
 import keras.backend as K
 
-from settings import run_folder, run_archive_folder
+from settings import run_folder  #, run_archive_folder
 
 class Gen_Model():
 	def __init__(self, reg_const, learning_rate, input_dim, output_dim):
